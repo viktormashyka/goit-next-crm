b36a9252f4eac82b7ebf0c704170d7b133045f75
viktormashyka <99292051+viktormashyka@users.noreply.github.com>
2024-12-02 11:45:24 +0000

    create components, add style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,13 @@
+import ActiveLabel from './components/active-label';
+import NotActiveLabel from './components/not-active-label';
+
 export default function Home() {
   return (
     <div>
       <main>
         <h1>Home page</h1>
+        <ActiveLabel>Active</ActiveLabel>
+        <NotActiveLabel>Not Active</NotActiveLabel>
       </main>
     </div>
   );
