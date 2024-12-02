d7daf0f5c48b2b4b6d5bae0926f5761e261b7f4a
viktormashyka <99292051+viktormashyka@users.noreply.github.com>
2024-12-02 14:46:06 +0000

    add clsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,9 @@ export default function Home() {
       <main>
         <h1 className="text-xl">Home page</h1>
         <StatusLabel status={Status.Active}>Active</StatusLabel>
-        <StatusLabel status={Status.NotActive}>Not Active</StatusLabel>
+        <StatusLabel status={Status.NotActive} disabled>
+          Not Active
+        </StatusLabel>
         <StatusLabel status={Status.Pending}>Pending</StatusLabel>
         <StatusLabel status={Status.Suspended}>Suspended</StatusLabel>
       </main>
