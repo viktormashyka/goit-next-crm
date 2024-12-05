9dc51b714d63b45c63946c8e5d48116624d31250
viktormashyka <99292051+viktormashyka@users.noreply.github.com>
2024-12-05 16:56:00 +0000

    clean up app and dashboard  pages

--- src/app/(admin)/dashboard/page.tsx
+++ src/app/(admin)/dashboard/page.tsx
@@ -1,11 +1,9 @@
-import MagicButton from '@/app/components/magic-button';
 import React from 'react';
 
 export default function Page() {
   return (
     <main>
       <h1 className="text-xl">Dashboard</h1>
-      <MagicButton />
     </main>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,21 @@
-import AddCompanyButton from './components/add-company-button';
-import MagicButton from './components/magic-button';
+'use client';
+
+import { useRouter } from 'next/navigation';
+import Button from './components/button';
 
 export default function Home() {
+  const router = useRouter();
   return (
     <div>
       <main>
         <h1 className="text-xl">Home page</h1>
-        <AddCompanyButton />
-        <MagicButton />
+        <Button
+          onClick={() => {
+            router.push('./dashboard');
+          }}
+        >
+          Navigate to Dashboard
+        </Button>
       </main>
     </div>
   );
